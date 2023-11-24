9a5668e67a3160991a49e4c497910923d87d3de6
afivve <afifdgr3@gmail.com>
2023-11-24 07:39:33 +0700

    Create: Seeder Role

---
+++ prisma/migrations/20231123234008_create_users_table/migration.sql
@@ -0,0 +1,19 @@
+-- CreateTable
+CREATE TABLE `users` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `uuid` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
+    `email` VARCHAR(191) NOT NULL,
+    `phone` INTEGER NOT NULL,
+    `password` VARCHAR(191) NOT NULL,
+    `reset_token` VARCHAR(191) NULL,
+    `photo_id` INTEGER NOT NULL,
+    `role_id` INTEGER NOT NULL,
+    `verified` BOOLEAN NOT NULL DEFAULT false,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updated_at` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `users_email_key`(`email`),
+    UNIQUE INDEX `users_phone_key`(`phone`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
--- prisma/seeder.js
+++ prisma/seeder.js
@@ -1,8 +1,31 @@
 const db = require('./connection')
 const { faker } = require('@faker-js/faker/locale/id_ID')
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcrypt")
+
+const usedRoleName = new Set()
 
 async function seedData()  {
+    /* Reset database before run seeder */
+    await db.$transaction([db.user.deleteMany()]);
+    await db.$transaction([db.role.deleteMany()]);
+    await db.$queryRaw`ALTER TABLE users AUTO_INCREMENT = 1`;
+    await db.$queryRaw`ALTER TABLE roles AUTO_INCREMENT = 1`;
+
+    /* Role Seeder */
+    for (let i = 0; i < 2; i++) {
+        let roleName;
+        do {
+          roleName = faker.helpers.arrayElement(['user', 'admin']);
+        } while (usedRoleName.has(roleName));
+    
+        usedRoleName.add(roleName);
+    
+        const seedRole = {
+          name: roleName,
+        }
+    
+        await db.role.create({ data: seedRole });
+    }
 
     /* User Seeder */
     for (let i = 0; i < 10; i++) {
@@ -10,7 +33,7 @@ async function seedData()  {
             uuid: faker.string.uuid(),
             name: faker.person.fullName(),
             email: faker.internet.email(),
-            phone: faker.phone.number(),
+            phone: faker.number.int({ max: 100000000 }),
             password: bcrypt.hashSync("12345678", bcrypt.genSaltSync(10)), 
             verified: faker.datatype.boolean(0.9),
             roleId: faker.number.int({ min: 1, max: 3 }), 
@@ -19,7 +42,7 @@ async function seedData()  {
         await db.user.create({ data: seedUser });
     }
 
-    /* Prisma Disconnect Connection */
+    /* Disconnect Prisma Connection */
     await db.$disconnect();
 }
 
