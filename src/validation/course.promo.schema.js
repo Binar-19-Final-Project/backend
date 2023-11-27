7f5ae90ee3906e8f4cd8d660aeb54b25fc980e15
izzanka <izzancoc.46@gmail.com>
2023-11-27 22:12:54 +0700

    Update: Change response validate message

--- src/validation/course.promo.schema.js
+++ src/validation/course.promo.schema.js
@@ -2,13 +2,13 @@ const { body } = require("express-validator")
 
 module.exports = {
   promo: [
-    body("name").notEmpty().withMessage("name is required"),
-    body("name").isString().withMessage("name must be type of string"),
-    body("discount").notEmpty().withMessage("discount is required"),
-    body("discount").isFloat().withMessage("discount must be type of number"),
-    body("discount").isFloat({min: 1, max: 99}).withMessage("discount must more than 1 and less than 100"),
-    body("expiredAt").notEmpty().withMessage("expired at is required"),
-    body("expiredAt").isISO8601().withMessage("expired at must be type of date time"),
-    body("expiredAt").isAfter(new Date().toDateString()).withMessage("expired at must after current date")
+    body("name").notEmpty().withMessage("Nama wajib diisi"),
+    body("name").isString().withMessage("Nama harus berupa karakter"),
+    body("discount").notEmpty().withMessage("Diskon wajib diisi"),
+    body("discount").isFloat().withMessage("Diskon harus berupa angka"),
+    body("discount").isFloat({min: 1, max: 99}).withMessage("Diskon harus lebih dari 0 dan kurang dari 100"),
+    body("expiredAt").notEmpty().withMessage("Tanggal kadaluarsa wajib diisi"),
+    body("expiredAt").isISO8601().withMessage("Tanggal kadaluarsa harus berupa tanggal dan waktu"),
+    body("expiredAt").isAfter(new Date().toDateString()).withMessage("Tanggal kadaluarsa harus melewati tanggal dan waktu sekarang")
   ],
 };
