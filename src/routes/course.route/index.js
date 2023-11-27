fe6f364c8c862b6514700c79d185598d0720f693
izzanka <izzancoc.46@gmail.com>
2023-11-27 17:04:18 +0700

    Create: Course promo routes

--- src/routes/course.route/index.js
+++ src/routes/course.route/index.js
@@ -1,7 +1,9 @@
 const express = require("express"),
     instructorRoute = require('./course.instructor'),
+    promoRoute = require('./course.promo'),
     router = express.Router()
     
 router.use('/course-instructor', instructorRoute)
+router.use('/course-promo', promoRoute)
 
 module.exports = router
