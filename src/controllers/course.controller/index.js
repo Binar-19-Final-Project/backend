978ff12d04e5e258695e656f31f90e2c86278fcb
taufiqhida <taufiq200305@gmail.com>
2023-11-28 22:05:18 +0700

    perbaikan couse type

--- src/controllers/course.controller/index.js
+++ src/controllers/course.controller/index.js
@@ -2,12 +2,14 @@ const course = require('./course'),
     instructor = require('./course.instructor'),
     promo = require('./course.promo'),
     courseModule = require('./course.module'),
-    courseCategory = require('./course.category')
+    courseCategory = require('./course.category'),
+    courseType = require("./course.type")
 
 module.exports = {
     course,
     instructor,
     promo,
     courseModule,
+    courseType,
     courseCategory
 }
