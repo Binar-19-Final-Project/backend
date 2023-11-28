36093ce0818766bcaa7401865657af4f90d55457
izzanka <izzancoc.46@gmail.com>
2023-11-28 11:13:48 +0700

    Update: Move course promo schema validation

--- src/validation/course.schema.js
+++ src/validation/course.schema.js
@@ -4,4 +4,15 @@ module.exports = {
   instructor: [
     body("name").notEmpty().withMessage("name is required")
   ],
+  
+  promo: [
+    body("name").notEmpty().withMessage("Nama wajib diisi"),
+    body("name").isString().withMessage("Nama harus berupa karakter"),
+    body("discount").notEmpty().withMessage("Diskon wajib diisi"),
+    body("discount").isFloat().withMessage("Diskon harus berupa angka"),
+    body("discount").isFloat({min: 1, max: 99}).withMessage("Diskon harus lebih dari 0 dan kurang dari 100"),
+    body("expiredAt").notEmpty().withMessage("Tanggal kadaluarsa wajib diisi"),
+    body("expiredAt").isISO8601().withMessage("Tanggal kadaluarsa harus berupa tanggal dan waktu"),
+    body("expiredAt").isAfter(new Date().toDateString()).withMessage("Tanggal kadaluarsa harus melewati tanggal dan waktu sekarang")
+  ],
 };
