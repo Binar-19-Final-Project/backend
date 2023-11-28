1d89431e8c2e911e32399c8f6109f3b6187e45c5
taufiqhida <taufiq200305@gmail.com>
2023-11-28 22:02:07 +0700

    perbaikan category

--- src/controllers/course.controller/index.js
+++ src/controllers/course.controller/index.js
@@ -1,11 +1,13 @@
 const course = require('./course'),
     instructor = require('./course.instructor'),
     promo = require('./course.promo'),
-    courseModule = require('./course.module')
+    courseModule = require('./course.module'),
+    courseCategory = require('./course.category')
 
 module.exports = {
     course,
     instructor,
     promo,
-    courseModule
+    courseModule,
+    courseCategory
 }
