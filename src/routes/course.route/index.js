7e1f690e1299627eebc0d441d91473e0332f65b2
taufiqhida <taufiq200305@gmail.com>
2023-11-28 21:30:43 +0700

    tambah router couse type

--- src/routes/course.route/index.js
+++ src/routes/course.route/index.js
@@ -3,11 +3,14 @@ const express = require("express"),
     courseRoute = require('./course'),
     promoRoute = require('./course.promo'),
     moduleRoute = require('./course.module'),
+    typeRoute = require("./course.type")
     router = express.Router()
     
 router.use('/course', courseRoute)
 router.use('/course-instructor', instructorRoute)
 router.use('/course-promo', promoRoute)
 router.use('/course-module', moduleRoute)
+router.use('/course-type', typeRoute)
+
 
 module.exports = router
