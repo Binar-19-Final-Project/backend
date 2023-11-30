1e85fb182bfb685ddd07861696ffa55135f28f27
izzanka <izzancoc.46@gmail.com>
2023-11-30 13:19:12 +0700

    Create: Route for course content

--- src/routes/course.route/index.js
+++ src/routes/course.route/index.js
@@ -3,11 +3,13 @@ const express = require("express"),
     courseRoute = require('./course'),
     promoRoute = require('./course.promo'),
     moduleRoute = require('./course.module'),
+    contentRoute = require('./course.content'),
     router = express.Router()
     
 router.use('/course', courseRoute)
 router.use('/course-instructor', instructorRoute)
 router.use('/course-promo', promoRoute)
 router.use('/course-module', moduleRoute)
+router.use('/course-content', contentRoute)
 
 module.exports = router
