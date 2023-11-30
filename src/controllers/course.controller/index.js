79f4cb4dd965e3fe29a7d690a3553001ce420600
izzanka <izzancoc.46@gmail.com>
2023-11-30 13:19:25 +0700

    Create: Crud for course content

--- src/controllers/course.controller/index.js
+++ src/controllers/course.controller/index.js
@@ -1,11 +1,13 @@
 const course = require('./course'),
     instructor = require('./course.instructor'),
     promo = require('./course.promo'),
-    courseModule = require('./course.module')
+    courseModule = require('./course.module'),
+    content = require('./course.content')
 
 module.exports = {
     course,
     instructor,
     promo,
-    courseModule
+    courseModule,
+    content
 }
