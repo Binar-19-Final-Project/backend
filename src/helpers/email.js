8712d8552fddf25b7fa28a9b49d6e559d69eba2b
izzanka <izzancoc.46@gmail.com>
2023-12-01 15:27:30 +0700

    Update: Email design for reset password

--- src/helpers/email.js
+++ src/helpers/email.js
@@ -1,4 +1,4 @@
-const { NODEMAILER_EMAIL } = require('../config')
+const { NODEMAILER_EMAIL, LOCAL_URL, RAILWAY_URL } = require('../config')
 
 module.exports = {
     register: async (email, otp) => {
@@ -20,20 +20,42 @@ module.exports = {
         }
     },
 
-    requestResetPassword: async (email, otp) => {
+    // requestResetPassword: async (email, otp) => {
+    //     return {
+    //         from: NODEMAILER_EMAIL,
+    //         to: email,
+    //         subject: "Reset Password Verification Code",
+    //         html:`
+    //         <div style="font-family: Arial, sans-serif; max-width: 600px; margin: 0 auto; padding: 20px; border: 1px solid #ddd; border-radius: 5px;">
+    //             <h2 style="color: #333; text-align: center;">Reset Password Verification</h2>
+    //             <p style="color: #555; text-align: center; font-size: 18px;">Your verification code:</p>
+    //             <div style="background-color: #f5f5f5; padding: 15px; text-align: center; font-size: 24px; border-radius: 5px;">
+    //                 <span style="color: tomato;"><b>${otp}</b></span>
+    //             </div>
+    //             <p style="color: #555; text-align: center;">This code expires in 10 minute(s).</p>
+    //             <p style="color: #FF0000; text-align: center;"><i>Don't share this verification code to anyone</i></p>
+    //         </div>
+    //     `,
+    //     }
+    // },
+
+    requestResetPassword: async (email, resetToken) => {
         return {
             from: NODEMAILER_EMAIL,
             to: email,
-            subject: "Reset Password Verification Code",
+            subject: "Reset Password",
             html:`
             <div style="font-family: Arial, sans-serif; max-width: 600px; margin: 0 auto; padding: 20px; border: 1px solid #ddd; border-radius: 5px;">
-                <h2 style="color: #333; text-align: center;">Reset Password Verification</h2>
-                <p style="color: #555; text-align: center; font-size: 18px;">Your verification code:</p>
+                <h2 style="color: #333; text-align: center;">Reset Password</h2>
                 <div style="background-color: #f5f5f5; padding: 15px; text-align: center; font-size: 24px; border-radius: 5px;">
-                    <span style="color: tomato;"><b>${otp}</b></span>
+                    <span style="color: tomato;">Click <a href="${LOCAL_URL}/reset-password/${resetToken}">here</a> to reset your password</span>
                 </div>
-                <p style="color: #555; text-align: center;">This code expires in 10 minute(s).</p>
-                <p style="color: #FF0000; text-align: center;"><i>Don't share this verification code to anyone</i></p>
+                <h2 style="color: #333; text-align: center;">Or copy this link</h2>
+                <div style="background-color: #f5f5f5; padding: 15px; text-align: center; font-size: 24px; border-radius: 5px;">
+                    <p>
+                        ${LOCAL_URL}/reset-password/${resetToken}
+                    </p>
+                </div>
             </div>
         `,
         }
