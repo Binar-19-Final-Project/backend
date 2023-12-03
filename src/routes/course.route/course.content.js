81cc38e5deae298b1ba8561177bee6ef82e0719b
afivve <afifdgr3@gmail.com>
2023-12-03 17:19:02 +0700

    Create: premium content middleware

--- src/routes/course.route/course.content.js
+++ src/routes/course.route/course.content.js
@@ -1,6 +1,8 @@
 const express = require('express'),
     schema = require('../../validation/course.schema'),
     validate = require('../../middlewares/validation'),
+    { verifyToken } = require('../../middlewares/verify.token'),
+    { premiumContent } = require('../../middlewares/premium.content'),
     controller = require('../../controllers/course.controller'),
     router = express.Router()
 
@@ -10,6 +12,6 @@ router.get('/content/:id', controller.courseContent.getById)
 router.put('/content/:id', validate(schema.content), controller.courseContent.update)
 router.delete('/content/:id', controller.courseContent.delete)
 
-router.get('/:courseId/module/:moduleId/content/:contentId', controller.courseContent.getCourseContentByIdModuleAndCourse)
+router.get('/:courseId/module/:moduleId/content/:contentId', verifyToken, premiumContent, controller.courseContent.getCourseContentByIdModuleAndCourse)
 
 module.exports = router
