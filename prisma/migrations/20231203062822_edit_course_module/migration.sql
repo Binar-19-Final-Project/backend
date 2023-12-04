733f6af66af04e36fb89425f326ec199db00ed6a
afivve <afifdgr3@gmail.com>
2023-12-04 10:50:24 +0700

    Update: schema prisma

--- prisma/migrations/20231203062822_edit_course_module/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `total_chapter` on the `course_modules` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE `course_modules` DROP COLUMN `total_chapter`;
