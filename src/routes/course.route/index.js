41a781642043f0c46ace4bf2dbd0dd15eb4458e7
afivve <afifdgr3@gmail.com>
2023-12-04 09:46:32 +0700

    Update

--- src/routes/course.route/index.js
+++ src/routes/course.route/index.js
@@ -1,19 +1,19 @@
 const express = require("express"),
-  instructorRoute = require("./course.instructor"),
-  courseRoute = require("./course"),
-  promoRoute = require("./course.promo"),
-  moduleRoute = require("./course.module"),
-  contentRoute = require("./course.content"),
-  typeRoute = require("./course.type"),
-  categoryRoute = require("./course.category"),
-  router = express.Router();
+    instructorRoute = require('./course.instructor'),
+    courseRoute = require('./course'),
+    promoRoute = require('./course.promo'),
+    moduleRoute = require('./course.module'),
+    contentRoute = require('./course.content'),
+    typeRoute = require("./course.type"),
+    categoryRoute = require("./course.category"),
+    router = express.Router()
 
-router.use("/course", courseRoute);
-router.use("/course-instructor", instructorRoute);
-router.use("/course-promo", promoRoute);
-router.use("/course-module", moduleRoute);
-router.use("/course-content", contentRoute);
-router.use("/course-type", typeRoute);
-router.use("/course-category", categoryRoute);
+router.use('/instructors', instructorRoute)
+router.use('/promos', promoRoute)
+router.use('/contents', contentRoute)
+router.use('/types', typeRoute)
+router.use('/categories', categoryRoute)
+router.use('/modules', moduleRoute)
+router.use('/', courseRoute)
 
 module.exports = router
