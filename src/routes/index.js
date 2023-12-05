d417abb3e0f0525bbf225aa97163095dd3c4a816
izzanka <izzancoc.46@gmail.com>
2023-12-05 20:16:56 +0700

    Create: Profile and image update

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,11 +3,13 @@ const express = require("express"),
     courseRoute = require('./course'),
     orderRoute = require('./order'),
     userCoursesRoute = require('./user.course'),
+    userProfileRoute = require('./user.profile'),
     router = express.Router()
     
 router.use("/auth", authRoute)
 router.use("/courses", courseRoute)
 router.use("/orders", orderRoute)
 router.use("/user-courses", userCoursesRoute)
+router.use("/profile", userProfileRoute)
 
 module.exports = router
