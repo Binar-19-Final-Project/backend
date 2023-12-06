1e01a99780d48ccb9af929e751bcad78a2e993d7
taufiqhida <taufiqh2003@gmail.com>
2023-12-06 07:32:43 +0700

    tambah level yang sempet kena confic

--- src/controllers/course/index.js
+++ src/controllers/course/index.js
@@ -1,17 +1,19 @@
-const course = require('./course.controller'),
-    courseInstructor = require('./course.instructor.controller'),
-    coursePromo = require('./course.promo.controller'),
-    courseModule = require('./course.module.controller'),
-    courseContent = require('./course.content.controller'),
-    courseCategory = require('./course.category.controller'),
-    courseType = require("./course.type.controller")
+const course = require("./course.controller"),
+  courseInstructor = require("./course.instructor.controller"),
+  coursePromo = require("./course.promo.controller"),
+  courseModule = require("./course.module.controller"),
+  courseContent = require("./course.content.controller"),
+  courseCategory = require("./course.category.controller"),
+  courseType = require("./course.type.controller"),
+  courseLevel = require("./course.level.controller");
 
 module.exports = {
-    course,
-    courseInstructor,
-    coursePromo,
-    courseModule,
-    courseContent,
-    courseCategory,
-    courseType
-}
+  course,
+  courseInstructor,
+  coursePromo,
+  courseModule,
+  courseContent,
+  courseCategory,
+  courseType,
+  courseLevel,
+};
--- src/routes/course/index.js
+++ src/routes/course/index.js
@@ -1,20 +1,21 @@
 const express = require("express"),
-    instructorRoute = require('./course.instructor.route'),
-    courseRoute = require('./course.route'),
-    promoRoute = require('./course.promo.route'),
-    moduleRoute = require('./course.module.route'),
-    contentRoute = require('./course.content.route'),
-    typeRoute = require("./course.type.route"),
-    categoryRoute = require("./course.category.route"),
-    router = express.Router()
+  instructorRoute = require("./course.instructor.route"),
+  courseRoute = require("./course.route"),
+  promoRoute = require("./course.promo.route"),
+  moduleRoute = require("./course.module.route"),
+  contentRoute = require("./course.content.route"),
+  typeRoute = require("./course.type.route"),
+  categoryRoute = require("./course.category.route"),
+  levelRoute = require("./course.level.route"),
+  router = express.Router();
 
-router.use('/instructors', instructorRoute)
-router.use('/promos', promoRoute)
-router.use('/types', typeRoute)
-router.use('/categories', categoryRoute)
-router.use('/contents', contentRoute)
-router.use('/modules', moduleRoute)
-router.use('/', courseRoute)
+router.use("/instructors", instructorRoute);
+router.use("/promos", promoRoute);
+router.use("/types", typeRoute);
+router.use("/categories", categoryRoute);
+router.use("/contents", contentRoute);
+router.use("/modules", moduleRoute);
+router.use("/level", levelRoute);
+router.use("/", courseRoute);
 
-
-module.exports = router
+module.exports = router;
