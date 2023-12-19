f74913d7c3178de88002bd0658adbbc697dac807
afivve <afifdgr3@gmail.com>
2023-12-19 19:44:42 +0700

    Create: return server error

--- src/middlewares/course.middleware.js
+++ src/middlewares/course.middleware.js
@@ -61,54 +61,59 @@ const courseCertificate = async (req, res, next) => {
 }
 
 const courseContentMiddleware = async (req, res, next) => {
-    const userCourse = await db.userCourse.findFirst({
-        where: {
-            userId: res.user.id,
-            courseId: parseInt(req.params.courseId)
-        }
-    })
-
-    const content = await db.courseContent.findFirst({
-        where: {
-            id: parseInt(req.params.contentId), 
-            moduleId: parseInt(req.params.moduleId), 
-            courseModule: {
-                    courseId: parseInt(req.params.courseId),
-                },
-        },
-        include: {
-            courseModule: true,
-            courseModule: {
-                include: {
-                    course: true
+    try {
+        const userCourse = await db.userCourse.findFirst({
+            where: {
+                userId: res.user.id,
+                courseId: parseInt(req.params.courseId)
+            }
+        })
+    
+        const content = await db.courseContent.findFirst({
+            where: {
+                id: parseInt(req.params.contentId), 
+                moduleId: parseInt(req.params.moduleId), 
+                courseModule: {
+                        courseId: parseInt(req.params.courseId),
+                    },
+            },
+            include: {
+                courseModule: true,
+                courseModule: {
+                    include: {
+                        course: true
+                    }
                 }
             }
-        }
-    })
-
-    const user = await db.user.findUnique({
-        where: {
-            id: res.user.id
-        }
-    })
-
-    const role = await db.role.findUnique({
-        where: {
-            id: user.roleId
-        }
-    })
-
-
-    if(content) {
-        if(content.isDemo === true) {
-            return next()
-        } else if(userCourse || role.name === 'admin') {
-            return next() 
+        })
+    
+        const user = await db.user.findUnique({
+            where: {
+                id: res.user.id
+            }
+        })
+    
+        const role = await db.role.findUnique({
+            where: {
+                id: user.roleId
+            }
+        })
+    
+    
+        if(content) {
+            if(content.isDemo === true) {
+                return next()
+            } else if(userCourse || role.name === 'admin') {
+                return next() 
+            } else {
+                return res.status(403).json(utils.apiError("Silahkan ambil atau order kelas ini terlebih dahulu"))
+            }
         } else {
-            return res.status(403).json(utils.apiError("Silahkan ambil atau order kelas ini terlebih dahulu"))
+            return res.status(404).json(utils.apiError("Content tidak ditemukan"))
         }
-    } else {
-        return res.status(404).json(utils.apiError("Content tidak ditemukan"))
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json(utils.apiError("Kesalahan pada internal server"))
     }
 }
 
