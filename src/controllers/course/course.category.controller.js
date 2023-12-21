9b070205c86799802e12cff904ee1b83fe78a62e
izzanka <izzancoc.46@gmail.com>
2023-12-21 21:33:17 +0700

    Update: Fix course category

--- src/controllers/course/course.category.controller.js
+++ src/controllers/course/course.category.controller.js
@@ -1,6 +1,6 @@
 const db = require("../../../prisma/connection"),
   utils = require("../../utils/utils"),
-  imageKit = require("../../utils/imageKit");
+  imageKitFile = require('../../utils/imageKitFile')
 
 module.exports = {
   getAll: async (req, res) => {
@@ -50,52 +50,48 @@ module.exports = {
         .json(utils.apiError("Kesalahan pada Internal Server "));
     }
   },
+
   create: async (req, res) => {
+
     try {
-      const { name, urlPhoto, isPublished } = req.body;
+
       const photoCategory = req.file;
-      console.log("test", req.body);
       const allowedMimes = [
         "image/png",
         "image/jpeg",
         "image/jpg",
         "image/webp",
       ];
-      if (!name) {
-        return res.status(400).json(utils.apiError("Nama Kosong1"));
-      }
       const allowedSizeMb = 2;
+
+      if(typeof photoCategory === 'undefined') return res.status(422).json(utils.apiError("Gambar kategori tidak boleh kosong"))
+
+      if(!allowedMimes.includes(photoCategory.mimetype)) return res.status(409).json(utils.apiError("Format gambar tidak diperbolehkan"))
+
+      if((photoCategory.size / (1024*1024)) > allowedSizeMb) return res.status(409).json(utils.apiError("Gambar kategori tidak boleh lebih dari 2mb"))
+
+      const uploadFile = await imageKitFile.upload(photoCategory)
+
+      if(!uploadFile) return res.status(500).json(utils.apiError("Kesalahan pada internal server"))
+
+      const { name, isPublished } = req.body;
+
       const nameSlug = await utils.createSlug(name);
-      if (typeof photoCategory === "undefined")
-        return res
-          .status(400)
-          .json(utils.apiError("Foto cover kategori tidak boleh kosong"));
-      if (!allowedMimes.includes(photoCategory.mimetype))
-        return res
-          .status(400)
-          .json(utils.apiError("cover kategori harus berupa gambar"));
-      if (photoCategory.size / (1024 * 1024) > allowedSizeMb)
-        return res
-          .status(400)
-          .json(utils.apiError("cover categori tidak boleh lebih dari 2mb"));
-      const stringFile = photoCategory.buffer.toString("base64");
-      const originalFileName = photoCategory.originalname;
-      const uploadFile = await imageKit.upload({
-        fileName: originalFileName,
-        file: stringFile,
-      });
-      const data = await db.courseCategory.create({
+      
+      await db.courseCategory.create({
         data: {
           name: name,
           slug: nameSlug,
           isPublished: Boolean(isPublished),
           urlPhoto: uploadFile.url,
+          imageFileName: uploadFile.name
         },
       });
 
       return res
         .status(200)
-        .json(utils.apiSuccess("Berhasil Menambah Kategori", data));
+        .json(utils.apiSuccess("Berhasil Menambah Kategori"));
+
     } catch (error) {
       console.log(error);
       return res
@@ -103,49 +99,63 @@ module.exports = {
         .json(utils.apiError("Kesalahan pada Internal Server"));
     }
   },
+
   update: async (req, res) => {
     try {
-      const { name, urlPhoto, isPublished } = req.body;
-      const nameSlug = await utils.createSlug(name);
+     
       const id = parseInt(req.params.id);
-      const photoCategory = req.file;
-      /* console.log(req.file, "gagal"); */
-      const allowedMimes = [
-        "image/png",
-        "image/jpeg",
-        "image/jpg",
-        "image/webp",
-      ];
-      const check = await db.courseCategory.findUnique({
+
+      const checkCategory = await db.courseCategory.findUnique({
         where: {
           id: id,
         },
       });
 
-      if (!check)
+      if (!checkCategory)
         return res
           .status(404)
           .json(utils.apiError("Kategori Tidak di temukan"));
-      const allowedSizeMb = 2;
-      /* if (typeof photoCategory === "undefined")
-        return res
-          .status(400)
-          .json(utils.apiError("Foto cover kategori tidak boleh kosong")); */
-      if (!allowedMimes.includes(photoCategory.mimetype))
-        return res
-          .status(400)
-          .json(utils.apiError("cover kategori harus berupa gambar"));
-      if (photoCategory.size / (1024 * 1024) > allowedSizeMb)
-        return res
-          .status(400)
-          .json(utils.apiError("cover categori tidak boleh lebih dari 2mb"));
-      const stringFile = photoCategory.buffer.toString("base64");
-      const originalFileName = photoCategory.originalname;
-      const uploadFile = await imageKit.upload({
-        fileName: originalFileName,
-        file: stringFile,
-      });
-      const category = await db.courseCategory.update({
+
+      const photoCategory = req.file
+      const allowedMimes = [
+          'image/png',
+          'image/jpeg',
+          'image/jpg',
+          'image/webp'
+      ]
+      const allowedSizeMb = 2
+
+      let imageUrl = null;
+      let imageFileName = null;
+
+      if(typeof photoCategory === 'undefined') {
+
+          imageUrl = checkCategory.imageUrl
+          imageFileName = checkCategory.imageFileName
+
+      }else{
+
+          if(!allowedMimes.includes(photoCategory.mimetype)) return res.status(409).json(utils.apiError("Format gambar tidak diperbolehkan"))
+
+          if((photoCategory.size / (1024*1024)) > allowedSizeMb) return res.status(409).json(utils.apiError("Gambar kelas tidak boleh lebih dari 2mb"))
+
+          if(checkCategory.imageFileName != null) {
+              const deleteFile = await imageKitFile.delete(checkCategory.imageFileName)
+              if(!deleteFile) return res.status(500).json(utils.apiError("Kesalahan pada internal server"))
+          }
+
+          const uploadFile = await imageKitFile.upload(photoCategory)
+
+          if(!uploadFile) return res.status(500).json(utils.apiError("Kesalahan pada internal server"))
+
+          imageUrl = uploadFile.url
+          imageFileName = uploadFile.name
+      }
+
+      const { name, isPublished } = req.body;
+      const nameSlug = await utils.createSlug(name);
+
+      await db.courseCategory.update({
         where: {
           id: id,
         },
@@ -153,13 +163,14 @@ module.exports = {
           name: name,
           slug: nameSlug,
           isPublished: Boolean(isPublished),
-          urlPhoto: uploadFile.url,
+          urlPhoto: imageUrl,
+          imageFileName: imageFileName
         },
       });
 
       return res
         .status(200)
-        .json(utils.apiSuccess("Berhasil mengubah kategori", category));
+        .json(utils.apiSuccess("Berhasil mengubah kategori"));
     } catch (error) {
       console.log(error);
       return res
@@ -167,6 +178,7 @@ module.exports = {
         .json(utils.apiError("Kesalahan pada Internal Server"));
     }
   },
+
   delete: async (req, res) => {
     try {
       const id = parseInt(req.params.id);
@@ -176,11 +188,14 @@ module.exports = {
           id: id,
         },
       });
-
+      
       if (!check)
         return res
           .status(404)
           .json(utils.apiError("Kategori tidak di temukan"));
+
+      const deleteFile = await imageKitFile.delete(check.imageFileName)
+      if(!deleteFile) return res.status(500).json(utils.apiError("Kesalahan pada internal server"))
 
       await db.courseCategory.delete({
         where: {
