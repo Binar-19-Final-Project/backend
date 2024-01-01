d9da8271a7d8cc95ec4bb8b74dbf5bd4ac08b5a2
afivve <afivve@gmail.com>
2024-01-01 19:21:48 +0700

    update

--- src/validation/course.schema.js
+++ src/validation/course.schema.js
@@ -5,6 +5,30 @@ module.exports = {
     body("name")
         .notEmpty().withMessage("Nama tidak boleh kosong")],
 
+   registerInstructor: [
+    body("name")
+        .notEmpty().withMessage("Nama tidak boleh kosong")
+        .matches(/^[a-zA-Z\s'-]+$/).withMessage("Nama tidak boleh mengandung karakter spesial")
+        .isString().withMessage("Nama harus berupa string"),
+    body("email")
+        .notEmpty().withMessage("Email tidak boleh kosong")
+        .isEmail().withMessage("Format email tidak valid"),
+    body("password")
+        .notEmpty().withMessage("Password tidak boleh kosong")
+        .isLength({ min: 8 }).withMessage("Password minimal 8 karakter")
+        .matches(/^(?=.*[A-Z])(?=.*\d).{8,}$/).withMessage("Password harus mengandung setidaknya satu huruf kapital dan satu angka")
+        .isString().withMessage("Password harus berupa string"),
+    body("confPassword")
+        .notEmpty().withMessage("Konfirmasi password tidak boleh kosong")
+        .custom((confPassword, { req }) => {
+            if (confPassword !== req.body.password) {
+                throw new Error("Password dan konfirmasi password tidak cocok");
+            }
+            return true;
+        })
+        .isString().withMessage("Konfirmasi password harus berupa string"),
+  ],
+
   course: [
     body("title").notEmpty().withMessage("Judul kelas tidak boleh kosong"),
     body("courseCategoryId").notEmpty().withMessage("Kategori kelas tidak boleh kosong"),
