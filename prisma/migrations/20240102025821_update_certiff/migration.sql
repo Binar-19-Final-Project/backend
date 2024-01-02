d6438bc8d3c90d77514eae7606f8c0ad8e857c96
afivve <afivve@gmail.com>
2024-01-02 12:01:26 +0700

    fix

--- prisma/migrations/20240102025821_update_certiff/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `addToLinkedin` to the `Certificate` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE `certificate` ADD COLUMN `addToLinkedin` VARCHAR(191) NOT NULL;
--- prisma/migrations/20240102031817_update_cewr/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE `certificate` MODIFY `addToLinkedin` LONGTEXT NOT NULL;
